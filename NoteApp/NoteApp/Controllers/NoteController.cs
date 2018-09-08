52a8056f8eb9691dca04c790795855af424f0b4b
Dani <daniel.schneider@abacus.ch>
2018-09-08 12:52:31 +0200

    Controlle und view für Note erstellt

---
+++ NoteApp/NoteApp/Controllers/NoteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoteApp.Controllers
+{
+    public class NoteController : Controller
+    {
+        public IActionResult Edit(int noteId)
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
