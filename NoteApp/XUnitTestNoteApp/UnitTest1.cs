d46dc8d0b8f56e96b6a456c48c87d48eaa43c7be
Eliseo <re.eliseo@gmail.com>
2018-09-15 19:05:44 +0200

    Unit Test Note updaten

--- NoteApp/XUnitTestNoteApp/UnitTest1.cs
+++ NoteApp/XUnitTestNoteApp/UnitTest1.cs
@@ -1,29 +1,24 @@
-using System;
 using Xunit;
 using NoteApp.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using NoteApp.Services;
 using System.Linq;
-using System.Collections.Generic;
-using System.Diagnostics;
-
-using System.Threading.Tasks;
-
-
-
+using NoteApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace XUnitTestNoteApp
 {
     public class UnitTest1
     {
 
-    private readonly ServiceProvider _serviceProvider;
-    private readonly NoteDBContext _dbContext;
+        private readonly ServiceProvider _serviceProvider;
+        private readonly NoteDBContext _dbContext;
+        private ServiceCollection services = new ServiceCollection();
 
-    public UnitTest1()
+        public UnitTest1()
         {
-            var services = new ServiceCollection();
+            services = new ServiceCollection();
             services.AddDbContext<NoteDBContext>(opt => opt.UseInMemoryDatabase());
             services.AddSingleton<IStyle>(sortOrder => new Style());
             services.AddSingleton<ISortOrder>(sortOrder => new SortOrder());
@@ -33,6 +28,10 @@ namespace XUnitTestNoteApp
             _serviceProvider = services.BuildServiceProvider();
             _dbContext = _serviceProvider.GetService<NoteDBContext>();
 
+            //services.AddMvc();
+
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
         }
 
         [Fact]
@@ -40,25 +39,47 @@ namespace XUnitTestNoteApp
         {
 
             Note note = Note.CreateNew();
-            note.Title = "Zweite Notiz";
-            note.Text = "Das ist der elende Text";
+            note.ID = 1;
+            note.Title = "Titel 1.Notiz";
+            note.Text = "Text der 1. Notiz";
 
             _dbContext.Add(note);
             _dbContext.SaveChanges();
+            var style = _serviceProvider.GetService<IStyle>();
+
+
+
+            var controllerTest = new NoteController(_dbContext, style);
+            IActionResult resultat = controllerTest.Edit(1);
+            resultat.ToString();
+            System.Diagnostics.Debug.WriteLine("RESULTAT " + resultat.ToString());
+
+            string modifizierterTitle = "Modifizierter Titel";
+
+            var modifiedNode = Note.CreateNew();
+            modifiedNode.ID = 1;
+            modifiedNode.Title = modifizierterTitle;
+            modifiedNode.Text = "Modifiziertet Text";
+            controllerTest.Edit(1, modifiedNode);
 
 
             var query = from n in _dbContext.Note
-                        where n.Title == "Zweite Notiz"
+                        where n.Title == modifizierterTitle
                         select n;
-            int count = (from x in _dbContext.Note select x).Count();
 
-           
-            System.Diagnostics.Debug.WriteLine("RESULTAT " + count);
+            var updatedNote = query.Single();
 
-            var noteSQlResultrat = query.Single();
+            //int count = (from x in _dbContext.Note select x).Count();
 
-            
-            System.Diagnostics.Debug.WriteLine("RESULTAT " + noteSQlResultrat.Text);
+            Assert.Equal(1, updatedNote.ID);
+            Assert.Equal(modifizierterTitle, updatedNote.Title);
+
+            //System.Diagnostics.Debug.WriteLine("RESULTAT " + count);
+
+            //var noteSQlResultrat = query.Single();
+
+
+            //System.Diagnostics.Debug.WriteLine("RESULTAT " + noteSQlResultrat.Text);
             //var data = _dbContext.Database.SqlQuery(query);
 
             //_dbContext.Note.FirstOrDefaultAsync
